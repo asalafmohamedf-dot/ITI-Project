f7f20e33e2ca24fc29608353c9a885bf7bf8f6b2
asala <asalafmohamedf@gmail.com>
2026-08-24 00:45:23 +0300

    task1,task2

---
+++ Day(12)/Task1/js/script.js
@@ -0,0 +1,21 @@
+var username = "Asala";
+var age = 20;
+var student = true;
+var x = null;
+var y;
+var grade = 85;
+if (grade >= 90) {
+    console.log("Excellent");
+}
+else if (grade >= 80) {
+    console.log("Good");
+}
+else if (grade >= 70) {
+    console.log("Average");
+}
+else if (grade >= 60) {
+    console.log("Pass");
+}
+else {
+    console.log("Fail");
+}
